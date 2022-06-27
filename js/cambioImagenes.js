a9031977bb1c7bdc34a852317e7c609afa6197f6
Cristian Cordoba <cordobac96@gmail.com>
2022-06-27 17:35:56 -0500

    terminación de la seccion de favaritos

--- js/cambioImagenes.js
+++ js/cambioImagenes.js
@@ -8,3 +8,33 @@ function evento(sw){
     document.getElementById('efecto').src = pic;
 }
 
+function evento1(sw){
+    var pic;
+    if (sw == 1) {
+        pic = "/img/slider_3.jpg";
+    }else{
+        pic = "/img/slider_2.jpg";
+    }
+    document.getElementById('efecto2').src = pic;
+}
+
+function evento2(sw){
+    var pic;
+    if (sw == 1) {
+        pic = "/img/slider_2.jpg";
+    }else{
+        pic = "/img/slider_3.jpg";
+    }
+    document.getElementById('efecto3').src = pic;
+}
+
+function evento3(sw){
+    var pic;
+    if (sw == 1) {
+        pic = "/img/slider_1.jpg";
+    }else{
+        pic = "/img/slider_3.jpg";
+    }
+    document.getElementById('efecto4').src = pic;
+}
+
