8277c48b661441c28732df05a939d95b034a8aaa
Cristian <cristian.cordoba01@uptc.edu.co>
2022-07-10 12:02:51 -0500

    mejora del script para hacer el cambio de imagenes

--- js/cambioImagenes.js
+++ js/cambioImagenes.js
@@ -1,42 +1,41 @@
 function evento(sw){
     var pic;
-    if (sw == 1) {
-        pic = "/img/slider_3.jpg";
-    }else{
-        pic = "/img/slider_1.jpg";
+    switch (sw) {
+        case 1:
+            pic = "/img/slider_3.jpg";
+            document.getElementById('efecto').src = pic;
+            break;
+        case 2:   
+            pic = "/img/slider_1.jpg";
+            document.getElementById('efecto').src = pic;
+            break;
+        case 3:
+            pic = "/img/slider_3.jpg";
+            document.getElementById('efecto1').src = pic;
+            break;
+        case 4:   
+            pic = "/img/slider_2.jpg";
+            document.getElementById('efecto1').src = pic;
+            break;
+        case 5:
+            pic = "/img/slider_2.jpg";
+            document.getElementById('efecto2').src = pic;
+            break;
+        case 6:   
+            pic = "/img/slider_3.jpg";
+            document.getElementById('efecto2').src = pic;
+            break;
+        case 7:
+            pic = "/img/slider_1.jpg";
+            document.getElementById('efecto3').src = pic;
+            break;
+        case 8:   
+            pic = "/img/slider_3.jpg";
+            document.getElementById('efecto3').src = pic;
+            break;         
+        default:
+            break;
     }
-    document.getElementById('efecto').src = pic;
 }
-
-function evento1(sw){
-    var pic;
-    if (sw == 1) {
-        pic = "/img/slider_3.jpg";
-    }else{
-        pic = "/img/slider_2.jpg";
-    }
-    document.getElementById('efecto2').src = pic;
-}
-
-function evento2(sw){
-    var pic;
-    if (sw == 1) {
-        pic = "/img/slider_2.jpg";
-    }else{
-        pic = "/img/slider_3.jpg";
-    }
-    document.getElementById('efecto3').src = pic;
-}
-
-function evento3(sw){
-    var pic;
-    if (sw == 1) {
-        pic = "/img/slider_1.jpg";
-    }else{
-        pic = "/img/slider_3.jpg";
-    }
-    document.getElementById('efecto4').src = pic;
-}
-
 
 
