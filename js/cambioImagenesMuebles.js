313cd27b4142674da818880a78dbfeef4b640d0e
Cristian <cristian.cordoba01@uptc.edu.co>
2022-07-17 12:25:41 -0500

    terminación del proyecto

--- js/cambioImagenesMuebles.js
+++ js/cambioImagenesMuebles.js
@@ -1,146 +1,103 @@
-function evento(sw){
-    var pic;
-    switch (sw) {
-        case 1:
-            pic = "/img/slider_3.jpg";
-            document.getElementById('efecto').src = pic;
-            break;
-        case 2:   
-            pic = "/img/slider_1.jpg";
-            document.getElementById('efecto').src = pic;
-            break;
-        case 3:
-            pic = "/img/slider_3.jpg";
-            document.getElementById('efecto1').src = pic;
-            break;
-        case 4:   
-            pic = "/img/slider_2.jpg";
-            document.getElementById('efecto1').src = pic;
-            break;
-        case 5:
-            pic = "/img/slider_2.jpg";
-            document.getElementById('efecto2').src = pic;
-            break;
-        case 6:   
-            pic = "/img/slider_3.jpg";
-            document.getElementById('efecto2').src = pic;
-            break;
-        case 7:
-            pic = "/img/slider_1.jpg";
-            document.getElementById('efecto3').src = pic;
-            break;
-        case 8:   
-            pic = "/img/slider_3.jpg";
-            document.getElementById('efecto3').src = pic;
-            break;         
-        default:
-            break;
-    }
-}
-
-
 function productosCambio(sw){
     var aux;
     switch (sw) {
         case 1:
-            aux = "/img/Asientos/imagen_2.jpg";
+            aux = "/img/Muebles/imagen_2.jpg";
             document.getElementById('efecto').src = aux;
             break;
         case 2:   
-            aux = "/img/Asientos/imagen_1.jpg";
+            aux = "/img/Muebles/imagen_1.jpg";
             document.getElementById('efecto').src = aux;
             break;
         case 3:
-            aux = "/img/Asientos/imagen_3.jpg";
+            aux = "/img/Muebles/imagen_3.jpg";
             document.getElementById('efecto1').src = aux;
             break;
         case 4:   
-            aux = "/img/Asientos/imagen_2.jpg";
+            aux = "/img/Muebles/imagen_2.jpg";
             document.getElementById('efecto1').src = aux;
             break;
         case 5:
-            aux = "/img/Asientos/imagen_1.jpg";
+            aux = "/img/Muebles/imagen_1.jpg";
             document.getElementById('efecto2').src = aux;
             break;
         case 6:   
-            aux = "/img/Asientos/imagen_3.jpg";
+            aux = "/img/Muebles/imagen_3.jpg";
             document.getElementById('efecto2').src = aux;
             break;
         case 7:
-            aux = "/img/Asientos/imagen_1.jpg";
+            aux = "/img/Muebles/imagen_1.jpg";
             document.getElementById('efecto3').src = aux;
             break;
         case 8:   
-            aux = "/img/Asientos/imagen_4.jpg";
+            aux = "/img/Muebles/imagen_4.jpg";
             document.getElementById('efecto3').src = aux;
             break;         
         case 9:
-            aux = "/img/Asientos/imagen_1.jpg";
+            aux = "/img/Muebles/imagen_1.jpg";
             document.getElementById('efecto4').src = aux;
             break;
         case 10:   
-            aux = "/img/Asientos/imagen_4.jpg";
+            aux = "/img/Muebles/imagen_4.jpg";
             document.getElementById('efecto4').src = aux;
             break;
         case 11:
-            aux = "/img/Asientos/imagen_2.jpg";
+            aux = "/img/Muebles/imagen_2.jpg";
             document.getElementById('efecto5').src = aux;
             break;
         case 12:   
-            aux = "/img/Asientos/imagen_4.jpg";
+            aux = "/img/Muebles/imagen_4.jpg";
             document.getElementById('efecto5').src = aux;
             break;
         case 13:
-            aux = "/img/Asientos/imagen_3.jpg";
+            aux = "/img/Muebles/imagen_3.jpg";
             document.getElementById('efecto6').src = aux;
             break;
         case 14:   
-            aux = "/img/Asientos/imagen_4.jpg";
+            aux = "/img/Muebles/imagen_4.jpg";
             document.getElementById('efecto6').src = aux;
             break;
         case 15:
-            aux = "/img/Asientos/imagen_1.jpg";
+            aux = "/img/Muebles/imagen_1.jpg";
             document.getElementById('efecto7').src = aux;
             break;
         case 16:   
-            aux = "/img/Asientos/imagen_4.jpg";
+            aux = "/img/Muebles/imagen_4.jpg";
             document.getElementById('efecto7').src = aux;
             break;         
         case 17:   
-            aux = "/img/Asientos/imagen_2.jpg";
+            aux = "/img/Muebles/imagen_2.jpg";
             document.getElementById('efecto8').src = aux;
             break;
         case 18:
-            aux = "/img/Asientos/imagen_4.jpg";
+            aux = "/img/Muebles/imagen_4.jpg";
             document.getElementById('efecto8').src = aux;
             break;
         case 19:   
-            aux = "/img/Asientos/imagen_3.jpg";
+            aux = "/img/Muebles/imagen_3.jpg";
             document.getElementById('efecto9').src = aux;
             break;
         case 20:
-            aux = "/img/Asientos/imagen_4.jpg";
+            aux = "/img/Muebles/imagen_4.jpg";
             document.getElementById('efecto9').src = aux;
             break;
         case 21:   
-            aux = "/img/Asientos/imagen_1.jpg";
+            aux = "/img/Muebles/imagen_1.jpg";
             document.getElementById('efecto10').src = aux;
             break;         
         case 22:   
-            aux = "/img/Asientos/imagen_4.jpg";
+            aux = "/img/Muebles/imagen_4.jpg";
             document.getElementById('efecto10').src = aux;
             break;         
         case 23:   
-            aux = "/img/Asientos/imagen_2.jpg";
+            aux = "/img/Muebles/imagen_2.jpg";
             document.getElementById('efecto11').src = aux;
             break;         
         case 24:   
-            aux = "/img/Asientos/imagen_4.jpg";
+            aux = "/img/Muebles/imagen_4.jpg";
             document.getElementById('efecto11').src = aux;
             break;                
         default:
             break;
     }
 }
-
-
